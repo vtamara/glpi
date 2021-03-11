4bb054fea5d3677a8b347272495e5020f852e525
adelaunay <delaunay.alexandre@gmail.com>
2021-03-11 14:42:26 +0100

    work on ticket form (solution and linked tickets)

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4486,9 +4486,11 @@ JAVASCRIPT;
       TemplateRenderer::getInstance()->display('components/itilobject/layout.html.twig', [
          'item'     => $this,
          'followup' => new ITILFollowup,
+         'solution' => new ITILSolution,
          'params'   => $options,
          'timeline' => $this->getTimelineItems(),
          'template' => $tt,
+         'ticket_ticket' => new Ticket_Ticket,
       ]);
 
       return true;
